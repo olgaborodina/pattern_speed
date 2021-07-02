7872490bf0ccb4ef54e418b26ed162b27a6da461
olgaborodina <ol.borodina@icloud.com>
2021-07-02 16:13:39 +0200

    Replace loops from the pixelation process by scipy func

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as scp
 
 def rotate_bar(beta, X, Y, VX, VY):
     X_ = X * np.cos(beta) - Y * np.sin(beta)
@@ -13,7 +14,6 @@ def incline_galaxy(i, X, Y, VX, VY):
     VZ_ = VY * np.sin(i)
     VY_ = VY * np.cos(i)
 
-
     X_ = X
     Z_ = Y * np.sin(i)
     Y_ = Y * np.cos(i)
@@ -24,3 +24,15 @@ def add_solid_body_rotation(X, Y, VX, VY, Omegap=0.4):
     VX -= np.sqrt(X ** 2 + Y ** 2) * Omegap * np.sin(PHI)
     VY += np.sqrt(X ** 2 + Y ** 2) * Omegap * np.cos(PHI)
     return VX, VY
+
+def mean_in_pixel(X, Y, step, values):
+    bounds_x = np.arange(X.min().round(1) - step, X.max().round(1) + step, step=step)
+    bounds_y = np.arange(Y.min().round(1) - step, Y.max().round(1) + step, step=step)
+    statistics, x_edge, y_edge, bins = scp.binned_statistic_2d(X.flatten(), Y.flatten(), values.flatten(),
+                                                               'mean', bins=[bounds_x, bounds_y])
+    return statistics
+
+def centers_of_pixel(X, Y, step):
+    bounds_x = np.arange(X.min().round(1) - step, X.max().round(1) + step, step=step)
+    bounds_y = np.arange(Y.min().round(1) - step, Y.max().round(1) + step, step=step)
+    return np.meshgrid(bounds_x[:-1] + step, bounds_y[:-1] + step, indexing='ij')
