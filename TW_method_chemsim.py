285ea507be091a181cad9518f2a56deb72c909b7
olgaborodina <ol.borodina@icloud.com>
2022-02-23 17:10:14 +0100

    Fix plots and add tests with SBR and smooth density field

--- TW_method_chemsim.py
+++ TW_method_chemsim.py
@@ -8,8 +8,13 @@ from matplotlib import cm
 import functions as f
 import pybar.pybar_tom as pybar
 from pathlib import Path
+from scipy import interpolate
 
-
+os.environ["PATH"] += os.pathsep + '/usr/local/texlive/2021/bin/universal-darwin'
+plt.rcParams["mathtext.fontset"] = "cm"
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams['font.family'] = 'STIXGeneral'
+plt.rc('text', usetex=True)
 matplotlib.use('Agg')
 
 path = '/Data/oborodina/internship/astro-node/chemistry_simulations/'
@@ -53,6 +58,37 @@ snapshot = Snapshot(isnap,path,halfbox=halfbox,halfboy=halfboy,halfboz=halfboz,a
 snapshot.read_full()                                   # read snapshot
 snapshot.rotate_full(omega*snapshot.t)    # rotate snapshot to align bar with y axis, minus viewing angle phi
 
+
+##########  for SBR
+# snapshot.vx, snapshot.vy = np.zeros_like(snapshot.vx), np.zeros_like(snapshot.vy)
+# snapshot.vx, snapshot.vy = f.add_solid_body_rotation(snapshot.x, snapshot.y, snapshot.vx, snapshot.vy, -omega)
+
+
+##########  for smooth density
+# density = snapshot.nCO
+# radius_grid = np.linspace(0, 130, 201)
+# density_grid = []
+# radius_i_1 = 0
+
+# for radius_i in np.concatenate((radius_grid[1:], [170])):
+#     mask = (np.sqrt(snapshot.x ** 2 + snapshot.y ** 2) > radius_i_1) & (np.sqrt(snapshot.x ** 2 + snapshot.y ** 2) < radius_i)
+#     density_grid.append(density[mask].mean())
+#     radius_i_1 = radius_i
+
+# plt.plot()
+# inter_density = interpolate.interp1d(radius_grid, np.array(density_grid), fill_value='extrapolate')
+
+# fig, ax = plt.subplots(figsize=(8, 6))
+# ax.scatter(radius_grid / 10, np.array(density_grid))
+# ax.set_yscale('log')
+# ax.set_xlabel(r'$r$ [kpc]')
+# ax.set_xlabel(r'density [$cm^{-2}$]')
+# plt.savefig(FIG_DIR / 'density_mean.png', dpi=300)
+
+# print('Calculated azimuthal mean')
+
+# density_tot = inter_density(np.sqrt(snapshot.x ** 2 + snapshot.y ** 2))
+
 ## psi bar rotation
 u_bar = [0, 0, 1]
 snapshot.rotate_3D(u_bar, psi_bar)
@@ -69,19 +105,21 @@ ys = np.arange(ymin,ymax+dy,dy)
 xbins = np.arange(xmin-dx/2,xmax+3*dx/2,dx)
 ybins = np.arange(ymin-dy/2,ymax+3*dy/2,dy)
 
+
 density_CO = snapshot.nCO 
 density_H2 = snapshot.nH2 
 density_HI = snapshot.nHI 
-# density = snapshot.nHI 
+density_tot = snapshot.nTOT
 
-w_CO    = (density_CO*snapshot.volumes) /(dx*dy)*(arepoLength) # weights for binning
-w_H2    = (density_H2*snapshot.volumes) /(dx*dy)*(arepoLength) # weights for binning
-w_HI    = (density_HI*snapshot.volumes) /(dx*dy)*(arepoLength) # weights for binning
-w    = w_CO.astype(float64) * np.nanmedian(w_H2) / np.nanmedian(w_CO) + w_HI.astype(float64)
+# combination
+# w_CO    = (density_CO*snapshot.volumes) /(dx*dy)*(arepoLength) # weights for binning
+# w_H2    = (density_H2*snapshot.volumes) /(dx*dy)*(arepoLength) # weights for binning
+# w_HI    = (density_HI*snapshot.volumes) /(dx*dy)*(arepoLength) # weights for binning
+# w    = w_CO.astype(float64) * np.nanmedian(w_H2) / np.nanmedian(w_CO) + w_HI.astype(float64)
 
-# print(w_CO, w_CO.max(), '\n', w_H2, w_H2.max(), '\n', w, w.max())
-#w    = (density*snapshot.volumes) /(dx*dy)*(arepoLength) # weights for binning
-snapshot.vx, snapshot.vy = f.add_solid_body_rotation(snapshot.x, snapshot.y, snapshot.vx, snapshot.vy, 400)
+w = (density_tot * snapshot.volumes) /(dx*dy)*(arepoLength) # weights for binning
+name = 'All gas density'
+
 
 gas_binned, bins = np.histogramdd(doublets, bins = (xbins,ybins),weights=w)     # surface density in cm^-2
 binned_vx, bins = np.histogramdd(doublets, bins = (xbins,ybins), weights=snapshot.vx * snapshot.masses)  
@@ -104,7 +142,7 @@ binned_vz = binned_vz.T / 100
 binned_x = binned_x.T / 10
 binned_y = binned_y.T / 10
 
-VR = binned_vz# vy * np.sin(i)
+VR = binned_vz # vy * np.sin(i)
 
 print('Begin building the plot')
 
@@ -119,7 +157,7 @@ extent  = np.array([xmin,xmax,ymin,ymax])*u
 cbarticks = np.logspace(-100,100,201)
 
 # total
-title   = 'Total'
+title   = name
 cmap    = 'viridis'
 cbarlbl = r'$[\rm cm^{-2}]$'
 levels  = np.logspace(16,np.log10(gas_binned.max()),256)
@@ -129,6 +167,9 @@ im      = ax.imshow(gas_binned,norm=norm,extent=extent,cmap=cmap,origin='lower',
 cbar    = fig.colorbar(im,ticks=cbarticks, format=ticker.FuncFormatter(fmt),shrink=0.8)
 cbar.set_label(cbarlbl,fontsize=14)
 ax.set_title(title,fontsize=20)
+ax.set_xlabel(r'$x\, \rm [kpc]$', fontsize=14)
+ax.set_ylabel(r'$y\, \rm [kpc]$', fontsize=14)
+
 fig.savefig(FIG_DIR / 'rotated.pdf',bbox_inches='tight',dpi=200)
 
 
@@ -142,13 +183,15 @@ extent  = np.array([xmin,xmax,ymin,ymax])*u
 cbarticks = np.logspace(-100,100,201)
 
 # total
-title   = r'V_{LOS}'
+title   = r'$V_{LOS}$'
 cmap    = 'viridis'
-cbarlbl = r'$[\rm 100 km/s]$'
+cbarlbl = r'$[\rm 100 \ km/s]$'
 im      = ax.imshow(VR,extent=extent,cmap=cmap,origin='lower',interpolation='nearest')
 cbar    = fig.colorbar(im, format=ticker.FuncFormatter(fmt),shrink=0.8)
 cbar.set_label(cbarlbl,fontsize=14)
 ax.set_title(title,fontsize=20)
+ax.set_xlabel(r'$x\, \rm [kpc]$', fontsize=14)
+ax.set_ylabel(r'$y\, \rm [kpc]$', fontsize=14)
 fig.savefig(FIG_DIR / 'rotated_vlos.pdf',bbox_inches='tight',dpi=200)
 
 #### TW Method begins
@@ -178,21 +221,21 @@ fig, ax = plt.subplots(figsize=(10, 8))
 levels  = np.logspace(16, np.log10(RHO_array.max()),256)
 norm    = mc.BoundaryNorm(levels,256)
 sc = ax.scatter(bar.X_lon, bar.Y_lon, c=bar.Flux, 
-                cmap='seismic', norm=norm, marker='s', s=4)
+                cmap='seismic', norm=norm, marker='s', s=2)
 ax.grid(ls='dashed')
 ax.tick_params(labelsize=20, direction='in')
 ax.set_xlabel(r'$x\, \rm [kpc]$', fontsize=20)
 ax.set_ylabel(r'$y\, \rm [kpc]$', fontsize=20)
-ax.set_ylim(-12, 12)
+ax.set_ylim(-10, 10)
 ax.set_aspect('1')
-ax.set_title(r'$Density$', fontsize=20)
+ax.set_title(name, fontsize=20)
 #plt.colorbar(sc, format='%g', label=r'$\rm 100\, km/s$')
-plt.colorbar(sc, format='%g', label=r'$\rm cm^{-3}$')
-plt.savefig(FIG_DIR / '3D_galaxy_density_all.png', dpi=200)
+plt.colorbar(sc, format='%g', label=r'$\rm cm^{-2}$')
+plt.savefig(FIG_DIR / '3D_galaxy_density_all.png', dpi=300)
 
 
 bar.tremaine_weinberg()
-print(bar.dfx_tw)
+# print(bar.dfx_tw)
 print('applied TW method')
 
 omega_bar, omega_bar_err, c, c_err, x_tw, v_tw, x_tw_err, v_tw_err = f.fit(bar)
@@ -201,7 +244,7 @@ plasma = cm.get_cmap('plasma', len(x_tw))
 colors = plasma.colors
 
 x_fit = np.linspace(np.nanmin(x_tw), np.nanmax(x_tw), 3)
-y_GT  = (x_fit * 40) * np.sin(i) + c * 100
+y_GT  = (x_fit * 40) * np.sin(i) # + c * 100
 y_fit = (x_fit * omega_bar * 100) + c * 100
 
 #print(bar.dfx_tw)
@@ -221,19 +264,27 @@ sc = ax1.scatter(bar.X_lon, bar.Y_lon, c=bar.Vel_s * 100,
 # sc = ax1.scatter(bar.X_lon, bar.Y_lon, c=bar.Flux, 
 #                  cmap='gray', norm=norm, s=1.3, alpha=0.4)
 for index, y in enumerate(bar.y_slits):
-    if index % 4 == 0:
-        ax1.plot([bar.X_lon.min(), bar.X_lon.max()], [y, y], c=colors[index])
+    if index % 5 == 0:
+        ax1.plot([bar.X_lon.min(), bar.X_lon.max()], [y, y], c=colors[index], lw=1.2)
 ax1.set_xlabel(r'$x$ [kpc]', fontsize=15)
 ax1.set_ylabel(r'$y$ [kpc]', fontsize=15)
-ax1.tick_params(labelsize=12, direction='in')
+ax1.tick_params(labelsize=12)
 # ax1.annotate(r'$t=%.1f\, \rm Myr$' % (t * to_Myr), xy=(0.03, 0.94),
 #              xycoords='axes fraction', fontsize=16, color='k')
-ax1.annotate(r'$i=%.1f ^{\circ}$' % (np.rad2deg(i)), xy=(0.03, 0.89),
-             xycoords='axes fraction', fontsize=16, color='k')
-ax1.annotate(r'$\psi_{bar}=%.1f ^{\circ}$' % (np.rad2deg(psi_bar)), xy=(0.03, 0.84),
-             xycoords='axes fraction', fontsize=16, color='k')
+
+bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.8)
+ax1.text(0.17, 0.89, r"$i=%.1f ^{\circ}$" % (np.rad2deg(i)) + " \n " \
+                     + r"$\psi_{bar}=%.1f ^{\circ}$" % (np.rad2deg(psi_bar)), 
+        ha="center", va="center", size=20, fontsize=16,
+        bbox=bbox_props, transform=ax1.transAxes)
+
+# ax1.annotate(r'$i=%.1f ^{\circ}$' % (np.rad2deg(i)), xy=(0.03, 0.89),
+#              xycoords='axes fraction', fontsize=16, color='k')
+# ax1.annotate(r'$\psi_{bar}=%.1f ^{\circ}$' % (np.rad2deg(psi_bar)), xy=(0.03, 0.84),
+#              xycoords='axes fraction', fontsize=16, color='k')
 
 ax1.set_ylim(bar.X_lon.min(), bar.X_lon.max())
+ax1.set_xlim(bar.X_lon.min(), bar.X_lon.max())
 ax1.set_aspect("1")
 cbar = plt.colorbar(sc, ax=ax1, orientation="horizontal", fraction=0.07, ticks=cbarticks)
 cbar.set_label(label=r'$V_{LOS}$ [km/s]', size=15)
@@ -254,8 +305,8 @@ for x, y, e1, e2, color in zip(x_tw, v_tw * 100, x_tw_err, v_tw_err * 100, color
 #             xycoords='axes fraction',
 #             xytext=(0.7, 0.1), fontsize=15)
 
-ax2.set_ylim(-50, 50)
-ax2.set_xlim(-1, 1)
+ax2.set_ylim(-60, 60)
+ax2.set_xlim(-1.5, 1.5)
 
 ax2.tick_params(labelsize=12,direction='in')
 ax2.set_xlabel(r' $\langle x \rangle$ [kpc]', fontsize=15)
